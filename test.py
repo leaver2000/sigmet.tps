a813e705a62e42963d35fe330dbaa7a13031890a
jason leaver <jason.leaver.developer@gmail.com>
2021-10-01 12:19:32 +0000

    stormTrackVector update

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ from use.probsevere import ProbSevere
 URL = "https://mrms.ncep.noaa.gov/data/ProbSevere/PROBSEVERE/"
 QUERY = "?C=M;O=D"
 
-ps = ProbSevere()
+
 
 
 def download_sample_json():
@@ -32,7 +32,19 @@ def read_sample_json():
             feats = fc['features']
             ps.set_features(valid_time=vt, features=feats)
 
-    print(ps.list_np_plots())
+    # print(ps.list_np_plots())
 
 
-read_sample_json()
+def read_sample_one():
+    with open('data/MRMS_PROBSEVERE_20210928_155238.json', 'r') as f:
+        fc = json.load(f)
+        vt = fc['validTime'][:-6].replace('_', '-')
+        feats = fc['features']
+        ps = ProbSevere(valid_time=vt, features=feats)
+
+
+    pprint(ps.feature_collection)
+read_sample_one()
+# a=[1,2,3]
+# ra=[np.array(x)for x in a]
+# print(ra)
--- use/probsevere.py
+++ use/probsevere.py
@@ -1,8 +1,56 @@
 import numpy as np
-from use.stormtrack import StormTrack
-stormy = StormTrack()
+# from pprint import pprint
+VERSION='3.0.1'
+TYPE='FeatureCollection'
+PRODUCT="probSevere:vectors"
+SOURCE="NOAA/NCEP Central Operations"
+
+class StormTrackVector:
+    _stv={}
+    def set_vector(self, _id=None,center=None, motion_east=None, motion_south=None ):
+        self._id=_id
+        self._center=center
+        me = float(motion_east)
+        mn = np.multiply(float(motion_south), -1)
+        self.motion = np.array([me, mn])
+        slope = np.divide(mn,me)
+
+        if _id in self._stv.keys():
+            pass
+        else:
+            self._stv[_id]={
+                "motion":self.motion,
+                "center":center,
+                "slope":slope,
+                "vector_space":self._space()
+            }
+
+    def _offsets(self):
+        offsettime = [900,1800,2700,3600]#m/s offset for 2,15,30,45,60
+        return[np.multiply(self.motion, x) for x in offsettime]
 
 
+    def _space(self):
+        lat,lon = self._center 
+        arr = []
+        R = 6378137
+        for dn, de in self._offsets():
+            # Coordinate offsets in radians
+            dLat = dn/R
+            radLat = np.multiply(np.pi, lat/180)  # np.pi*lat/180
+            dLon = de/(R*np.cos(radLat))
+            # OffsetPosition, decimal degrees
+            latO = lat + dLat * 180/np.pi
+            lonO = lon + dLon * 180/np.pi
+            arr.append([latO, lonO])
+        return arr
+
+    def _multi_line_string(self):
+        mls = self._stv[self._id]
+        return [mls['vector_space']]
+
+
+stv = StormTrackVector()
 class ProbSevere:
     """
     The probsevere class maintains a persitance instance on the sever and accepts no __init__ fn arguments.
@@ -16,17 +64,21 @@ class ProbSevere:
 
 
     """
-
-    def set_features(self, valid_time=None, features=None):
-        _features = list()
-        self.feature_collection = dict(version='3.0.1', type='FeatureCollection', validtime=valid_time, features=_features,
-                                       product='ProbSevere:track', source="NOAA/NCEP Central Operations")
+    def __init__(self, valid_time=None, features=None):
+        _features = []
+        self.feature_collection={
+            'version':VERSION,
+            'type':TYPE,
+            'validtime':valid_time,
+            'product':PRODUCT,
+            'source':SOURCE,
+            'features':_features,
+        }
         for feat in features:
-            _features.append(self._reduced_json(feat))
-
+            _features.append(self._reduce(feat))
         return
 
-    def _reduced_json(self, feat):
+    def _reduce(self, feat):
         # PROPS
         geom = feat['geometry']
         props = feat['properties']
@@ -34,91 +86,51 @@ class ProbSevere:
         # CRDS
         crds = geom['coordinates']
         lons, lats = np.rollaxis(np.array(crds), 2, 0)
-        geometries = list()
+        geometries = []
         geometries.append(geom)
 
-        ################################|   CENTROID    |############################################
-        centroid = dict(type="point", coordinates=[
-                        np.mean(lons), np.mean(lats)])
-        geometries.append(centroid)
-
-        ################################|   SpaghettiStormTracks    |############################################
+        ################################|   MEAN CENTER    |############################################
+        center = [np.mean(lons), np.mean(lats)]
+        geometries.append({
+            'type':'point',
+            'coordinates':center
+        })
+        
+        ################################|   MultiLineString    |############################################
         storm_id = props['ID']
         mtn_s = props['MOTION_SOUTH']
         mtn_e = props['MOTION_EAST']
-        wnd = props['MEANWIND_1-3kmAGL']
-        storm_tracks = stormy.set_motion(
-            storm_id=storm_id, motion_east=mtn_e, motion_south=mtn_s, wnd=wnd, crds=[lons, lats])
-        # print(spaghetti_tracks)
-        ssts = dict(type="MultiLineString",
-                    name="SpaghettiStormTracks", coordinates=storm_tracks)
+        stv.set_vector(_id=storm_id, motion_east=mtn_e, motion_south=mtn_s,center=center)
+        mls = stv._multi_line_string()
+        ssts= {
+            'type':'MultiLineString',
+            'coordinates':mls
+        }
+
         geometries.append(ssts)
 
         ################################|   PROB SEVERE POLLYGON    |############################################
-
-        del feat['geometry']
-        feat['geometry'] = dict(
-            type="GeometryCollection", geometries=geometries)
-
+        feat['geometry'] ={
+            'geometries':geometries
+        }
         del feat['models']
         return feat
 
-    def list_np_plots(self):
-        return stormy.spaghetti_plots
-
-    # def _reduce(self, feat):
-    #     pass
-
-    # def _iterate(self, arr):
-    #     for value in arr:
-    #         # print(value)
-    #         self.features.append(handle_feature(value[6]))
 
 
-def linear_track(crds):
-    return crds
+
+# def linear_track(crds):
+#     return crds
 
 
-def parametrization_track(crds):
-    return crds
+# def parametrization_track(crds):
+#     return crds
 
 
-def get_storm_motion(mtn_s, mtn_e, mean_w):
-    lt = linear_track([mtn_s, mtn_e, mean_w])
-    pt = parametrization_track([mtn_s, mtn_e, mean_w])
-    return [lt, pt]
+# def get_storm_motion(mtn_s, mtn_e, mean_w):
+#     lt = linear_track([mtn_s, mtn_e, mean_w])
+#     pt = parametrization_track([mtn_s, mtn_e, mean_w])
+#     return [lt, pt]
 
 
-# def reduce_feature(feat):
-#     # PROPS
-#     geom = feat['geometry']
-#     props = feat['properties']
-#     props['MODELS'] = feat['models']['probsevere']['LINE01']
-#     # CRDS
-#     crds = geom['coordinates']
-#     lons, lats = np.rollaxis(np.array(crds), 2, 0)
-#     geometries = list()
-#     geometries.append(geom)
 
-#     ################################|   CENTROID    |############################################
-#     centroid = dict(type="point", coordinates=[np.mean(lons), np.mean(lats)])
-#     geometries.append(centroid)
-
-#     ################################|   STORM MOTION    |############################################
-#     # storm_id = props['ID']
-#     # mtn_s = props['MOTION_SOUTH']
-#     # mtn_e = props['MOTION_EAST']
-#     # wnd = props['MEANWIND_1-3kmAGL']
-#     # storm_motion = stormy.set_motion(
-#     #     storm_id=storm_id, motion_east=mtn_e, motion_south=mtn_s, wnd=wnd, crds=[lons, lats])
-#     # geometries.append(dict(type="MultiLineString",name="SpaghettiStormTrack" coordinates=storm_motion))
-
-#     ################################|   PROB SEVERE POLLYGON    |############################################
-#     # geom['track'] = get_storm_motion(
-#     #     props['MOTION_SOUTH'], props['MOTION_EAST'], props['MEANWIND_1-3kmAGL'])
-
-#     del feat['geometry']
-#     feat['geometry'] = dict(type="GeometryCollection", geometries=geometries)
-
-#     del feat['models']
-#     return feat
